6653a4b56783860da150ff42be44db84cdccd975
MAYANK AGARWAL <MAYANK AGARWAL@DESKTOP-DE7NOKL>
2025-10-23 01:59:58 +0530

    JAR_TO_WAR

--- ecom/src/main/java/com/example/jpa/EcomApplication.java
+++ ecom/src/main/java/com/example/jpa/EcomApplication.java
@@ -3,12 +3,18 @@ package com.example.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
-public class EcomApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(EcomApplication.class, args);
-	}
+public class EcomApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcomApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomApplication.class, args);
+    }
 }
